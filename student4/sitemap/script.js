5d731824ff23cf447ba73eadda97f6688f1a3bfd
Saranga99 <thathsarasaranga12345@gmail.com>
2021-03-15 03:04:51 +0530

    logo

---
+++ student4/sitemap/script.js
@@ -0,0 +1,9 @@
+document.getElementById("home").addEventListener("click", () => {
+    document.location.href = "../../gallery";
+});
+document.getElementById("products").addEventListener("click", () => {
+    document.location.href = "../../gallery";
+});
+document.getElementById("gallery").addEventListener("click", () => {
+    document.location.href = "../../gallery";
+});
